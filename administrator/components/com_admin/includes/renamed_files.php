b57783131b44fff5dcd43dce391227a2af04be0e
Richard Fath <richard67@users.noreply.github.com>
2021-06-26 16:50:48 +0200

    Move lists of deleted and renamed files and folders from script.php to separate PHP files

---
+++ administrator/components/com_admin/includes/renamed_files.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_admin
+ *
+ * @copyright   (C) 2011 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+$renamedFiles = [
+	// 3.10 changes
+	'/libraries/src/Filesystem/Support/Stringcontroller.php' => '/libraries/src/Filesystem/Support/StringController.php',
+	'/libraries/src/Form/Rule/SubFormRule.php' => '/libraries/src/Form/Rule/SubformRule.php',
+	// 4.0.0
+	'/media/vendor/skipto/js/skipTo.js' => '/media/vendor/skipto/js/skipto.js',
+];
