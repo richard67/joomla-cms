18c864b69d050bdf3e44427e44c3de08dd026593
Richard Fath <richard67@users.noreply.github.com>
2021-12-23 13:52:10 +0100

    Move deleted files and foldfer from script.php to separate PHP files

---
+++ administrator/components/com_admin/src/Script/RenamedFiles.php
@@ -0,0 +1,12 @@
+<?php
+namespace Joomla\Component\Admin\Administrator\Script;
+
+\defined('_JEXEC') or die;
+
+class RenamedFiles {
+	public $files = [
+		'/libraries/src/Filesystem/Support/Stringcontroller.php' => '/libraries/src/Filesystem/Support/StringController.php',
+		'/libraries/src/Form/Rule/SubFormRule.php' => '/libraries/src/Form/Rule/SubformRule.php',
+		'/media/vendor/skipto/js/skipTo.js' => '/media/vendor/skipto/js/skipto.js',
+	];
+}
