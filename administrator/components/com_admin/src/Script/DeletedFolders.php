996a35697f40bf1de94932da7aa0a3667d73e074
Richard Fath <richard67@users.noreply.github.com>
2021-12-26 19:36:09 +0100

    Code style and doc blocks for new PHP files

--- administrator/components/com_admin/src/Script/DeletedFolders.php
+++ administrator/components/com_admin/src/Script/DeletedFolders.php
@@ -1,9 +1,29 @@
 <?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_admin
+ *
+ * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
 namespace Joomla\Component\Admin\Administrator\Script;
 
 \defined('_JEXEC') or die;
 
-class DeletedFolders {
+/**
+ * Deleted folders registry for the script file of Joomla CMS
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class DeletedFolders
+{
+	/**
+	 * The list of folders to be deleted on CMS update
+	 *
+	 * @var    array
+	 * @since  __DEPLOY_VERSION__
+	 */
 	public $folders = [
 		'0' => '/templates/system/images',
 		'1' => '/templates/system/html',
--- administrator/components/com_admin/src/Script/RenamedFiles.php
+++ administrator/components/com_admin/src/Script/RenamedFiles.php
@@ -1,9 +1,29 @@
 <?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_admin
+ *
+ * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
 namespace Joomla\Component\Admin\Administrator\Script;
 
 \defined('_JEXEC') or die;
 
-class RenamedFiles {
+/**
+ * Renamed files registry for the script file of Joomla CMS
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class RenamedFiles
+{
+	/**
+	 * The list of files to be renamed on CMS update
+	 *
+	 * @var    array
+	 * @since  __DEPLOY_VERSION__
+	 */
 	public $files = [
 		'/libraries/src/Filesystem/Support/Stringcontroller.php' => '/libraries/src/Filesystem/Support/StringController.php',
 		'/libraries/src/Form/Rule/SubFormRule.php' => '/libraries/src/Form/Rule/SubformRule.php',
