c975277a79435957252b03ba0c15ebf52fad661b
Richard Fath <richard67@users.noreply.github.com>
2022-01-09 12:11:39 +0100

    Fix empty params for extensions on update

--- administrator/components/com_admin/sql/updates/postgresql/4.1.0-2022-01-08.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.1.0-2022-01-08.sql
@@ -1,3 +1,5 @@
 UPDATE "#__mail_templates"
    SET "params" = '{"tags": ["task_id", "task_title"]}'
  WHERE "template_id"= 'plg_system_tasknotification.orphan_mail' AND "params" = '{"tags": ["task_id", "task_title", ""]}';
+
+UPDATE "#__extensions" SET "params" = '{}' WHERE "params" = '';
