0ba62c4019ef749331b0b4d532498c526173f4b1
Richard Fath <richard.fath@t-online.de>
2024-01-27 11:49:48 +0100

    Enlarge name and title in assets table on update

---
+++ administrator/components/com_admin/sql/updates/mysql/5.0.3-2024-01-28.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `#__assets` MODIFY `name` varchar(180) NOT NULL;
+ALTER TABLE `#__assets` MODIFY `title` varchar(255) NOT NULL;
---
+++ administrator/components/com_admin/sql/updates/postgresql/5.0.3-2024-01-28.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "#__assets" ALTER COLUMN "name" TYPE character varying(180);
+ALTER TABLE "#__assets" ALTER COLUMN "title" TYPE character varying(255);
