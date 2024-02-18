937f97f3842295f8bef09756decd929589308c04
Richard Fath <richard.fath@t-online.de>
2024-02-18 20:27:51 +0100

    Set column value on update

--- administrator/components/com_admin/sql/updates/mysql/5.1.0-2024-02-10.sql
+++ administrator/components/com_admin/sql/updates/mysql/5.1.0-2024-02-10.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `#__guidedtour_steps` ADD COLUMN `params` text NOT NULL /** CAN FAIL **/;
+UPDATE `#__guidedtour_steps` SET `params` = '{}' WHERE `params` = '';
--- administrator/components/com_admin/sql/updates/postgresql/5.1.0-2024-02-10.sql
+++ administrator/components/com_admin/sql/updates/postgresql/5.1.0-2024-02-10.sql
@@ -1,1 +1,3 @@
-ALTER TABLE "#__guidedtour_steps" ADD COLUMN "params" text NOT NULL /** CAN FAIL **/;
+ALTER TABLE "#__guidedtour_steps" ADD COLUMN "params" text /** CAN FAIL **/;
+UPDATE "#__guidedtour_steps" SET "params" = '{}' WHERE "params" IS NULL;
+ALTER TABLE "#__guidedtour_steps" ALTER COLUMN "params" SET NOT NULL;
